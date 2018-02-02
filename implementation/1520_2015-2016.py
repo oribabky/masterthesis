d7996ed0a92d6172a8ed58d64fe15833cd871fe3
Tobias <tobias91.axelsson@gmail.com>
2018-02-02 09:59:41 +0100

    laptop

--- implementation/1520_2015-2016.py
+++ implementation/1520_2015-2016.py
@@ -13,15 +13,23 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 
+from sklearn.linear_model import LinearRegression
 #own library
 import customlib
+
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import datasets, linear_model
+from sklearn.metrics import mean_squared_error, r2_score
 
 # Load dataset
 #xl = pd.ExcelFile("../data/stations_e6/mätvärden 1520 2015-2016.xlsx")
 dataset = pandas.read_excel(open('../data/stations_e6/mätvärden 1520 2015-2016.xlsx','rb'), sheet_name='Blad1', skiprows=[0], header=1, 
-    names=['År', 'Tidpunkt', 'Yttemp-MS4', 'Nedtyp-MS4', 'Ned mängd-MS4', 'Yttemp-DST111', 'Friktion-DSC111', 'Ytstatus-DSC111'])
+    names=['År', 'Tidpunkt', 'Yttemp-MS4', 'Nedtyp-MS4', 'Ned mängd-MS4', 'Yttemp-DST111', 'Friktion-DSC111', 'Ytstatus-DSC111'],
+    #dtype={'a':np.int32, 'b':np.int32, 'c':np.float64, 'd':np.int32, 'e':np.float64, 'f':np.float64, 'g':np.float64, 'h':np.int32}
+    dtype=object
+    )
 #names = År Tidpunkt    Yttemp-MS4  Nedtyp-MS4  Ned mängd-MS4   Yttemp-DST111   Friktion-DSC111 Ytstatus-DSC111
-
 
 
 
@@ -29,7 +37,7 @@ dataset = pandas.read_excel(open('../data/stations_e6/mätvärden 1520 2015-2016
 #print(dataset.shape)
 
 # head
-#print(dataset.head(20))
+print(dataset.head(20))
 
 # descriptions
 #print(dataset.describe())
@@ -50,8 +58,56 @@ dataset = pandas.read_excel(open('../data/stations_e6/mätvärden 1520 2015-2016
 # scatter plot matrix
 #scatter_matrix(dataset)
 #plt.show()
+def runModelsRegression():
+    array = dataset.values
+    X = array[:,3:]
+    #X = customlib.sliceSkip2d(array, [0,1,2])
+    Y = array[:,2]
 
-def runModels():
+    #split the data into training/test data
+    percentageTrain = 51
+    X_split = customlib.splitData(percentageTrain, X)
+    X_train = X_split[0]
+    X_test = X_split[1]
+
+    Y_split = customlib.splitData(percentageTrain, Y)
+    Y_train = Y_split[0]
+    Y_test = Y_split[1]
+
+    array_split = customlib.splitData(percentageTrain, array)
+    array_train = array_split[0]
+    array_test = array_split[1]
+
+    # Create linear regression object
+    regr = linear_model.LinearRegression()
+    
+    # Train the model using the training sets
+    regr.fit(X_train, Y_train)
+
+    # Make predictions using the testing set
+    Y_pred = regr.predict(X_test)
+
+    # The coefficients
+    print('Coefficients: \n', regr.coef_)
+    # The mean squared error
+    print("Mean squared error: %.2f"
+          % mean_squared_error(Y_test, Y_pred))
+    # Explained variance score: 1 is perfect prediction
+    print('Variance score: %.2f' % r2_score(Y_test, Y_pred))
+
+    # Plot outputs
+    print(array_test[:5,1])
+    print(X_test[:5,1])
+    plt.scatter(array_test[:,1], Y_test,  color='black')
+    plt.plot(array_test[:,1], Y_pred, color='blue', linewidth=3)
+
+    #uncomment to hide graph value information
+    #plt.xticks(())
+    #plt.yticks(())
+
+    plt.show()
+
+def runModelsClassification():
     # Split-out validation dataset
     array = dataset.values
     X = customlib.sliceSkip2d(array, [2])
@@ -99,4 +155,5 @@ def runModels():
     print(confusion_matrix(Y_validation, predictions))
     print(classification_report(Y_validation, predictions))
 
-runModels()
+#runModels()
+runModelsRegression()
--- implementation/customlib.py
+++ implementation/customlib.py
@@ -11,4 +11,17 @@ def sliceSkip2d(list, skips):
         resultArray.append(innerArray)
     return resultArray
 
+def splitData(percentageTraining, inputList):
+    resultArray = []
+
+    size = len(inputList)
+    nrTrain = int( round(percentageTraining*size/100) )
+    trainArray = inputList[:nrTrain]
+    testArray = inputList[nrTrain:]
+
+    resultArray.append(trainArray)
+    resultArray.append(testArray)
+    return resultArray
+
+#print(splitData(50, [1,2,3,4,5,6,7,8,9]))
 #print(sliceSkip2d([[1,2,3],[4,5,6],[7,8,9]], [1]))
