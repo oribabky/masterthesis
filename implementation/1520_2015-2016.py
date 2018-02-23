d2068a36efdca1c8e28e8cd58e23ca739c6ed340
Tobias <tobias91.axelsson@gmail.com>
2018-02-23 09:54:51 +0100

    laptop

--- implementation/1520_2015-2016.py
+++ implementation/1520_2015-2016.py
@@ -24,13 +24,16 @@ from sklearn.metrics import mean_squared_error, r2_score
 
 # Load dataset
 #xl = pd.ExcelFile("../data/stations_e6/mätvärden 1520 2015-2016.xlsx")
-dataset = pandas.read_excel(open('../data/stations_e6/mätvärden 1520 2015-2016.xlsx','rb'), sheet_name='dsc111_noerr', skiprows=[0], header=1, 
+dataset = pandas.read_excel(open('../data/stations_e6/mätvärden total.xlsx','rb'), sheet_name='dsc111_noerr', skiprows=[0], header=1, 
     names=['År', 'Tidpunkt', 'Yttemp-MS4', 'Nedtyp-MS4', 'Ned mängd-MS4', 'Yttemp-DST111', 'Friktion-DSC111', 'Ytstatus-DSC111'],
-    dtype={'a':np.int32, 'b':np.int32, 'c':np.float64, 'd':np.int32, 'e':np.float64, 'f':np.float64, 'g':np.float64, 'h':np.int32}
+    dtype={'a':np.int64, 'b':np.int64, 'c':np.float64, 'd':np.int64, 'e':np.float64, 'f':np.float64, 'g':np.float64, 'h':np.int64}
     #dtype=object
     )
 #names = År Tidpunkt    Yttemp-MS4  Nedtyp-MS4  Ned mängd-MS4   Yttemp-DST111   Friktion-DSC111 Ytstatus-DSC111
-
+#dataset = pandas.read_csv('../data/stations_e6/mätvärden total.csv', 
+#    names=['År', 'Tidpunkt', 'Yttemp-MS4', 'Nedtyp-MS4', 'Ned mängd-MS4', 'Yttemp-DST111', 'Friktion-DSC111', 'Ytstatus-DSC111'],
+#    low_memory=False,
+#    dtype={'a':np.int32, 'b':np.int32, 'c':np.float64, 'd':np.int32, 'e':np.float64, 'f':np.float64, 'g':np.float64, 'h':np.int32})
 
 
 # shape
@@ -43,7 +46,7 @@ dataset = pandas.read_excel(open('../data/stations_e6/mätvärden 1520 2015-2016
 #print(dataset.describe())
 
 # class distribution
-#print(dataset.groupby('Nedtyp-MS4').size())
+print(dataset.groupby('Ytstatus-DSC111').size())
 
 #print(dataset.values)
 # box and whisker plots
@@ -58,6 +61,62 @@ dataset = pandas.read_excel(open('../data/stations_e6/mätvärden 1520 2015-2016
 # scatter plot matrix
 #scatter_matrix(dataset[2:])
 #plt.show()
+
+
+def modelSurfaceStatus():
+    # Split-out validation dataset
+    array = dataset.values
+    X = customlib.sliceSkip2d(array, [0,2,6,7])
+    Y = array[:,7]  
+    validation_size = 0.2
+    seed = 7
+    X_train, X_validation,Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
+
+    # Test options and evaluation metric
+    seed = 7
+    scoring = 'accuracy'
+
+    # Spot Check Algorithms
+    models = []
+    models.append(('LR', LogisticRegression()))
+    models.append(('LDA', LinearDiscriminantAnalysis()))
+    models.append(('KNN', KNeighborsClassifier()))
+    models.append(('CART', DecisionTreeClassifier()))
+    models.append(('NB', GaussianNB()))
+    #models.append(('SVM', SVC())) # long run time...
+    # evaluate each model in turn
+    results = []
+    names = []
+    for name, model in models:
+        kfold = model_selection.KFold(n_splits=10, random_state=seed)
+        cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+        results.append(cv_results)
+        names.append(name)
+        msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+        print(msg)
+
+    # Compare Algorithms
+    fig = plt.figure()
+    fig.suptitle('Algorithm Comparison')
+    ax = fig.add_subplot(111)
+    plt.boxplot(results)
+    ax.set_xticklabels(names)
+    plt.show()
+
+    # Make predictions on validation dataset
+    #knn = KNeighborsClassifier()
+    #knn.fit(X_train, Y_train)
+    #predictions = knn.predict(X_validation)
+    for name, model in models:
+        #cart = DecisionTreeClassifier()
+        print(name)
+        model.fit(X_train, Y_train)
+        predictions = model.predict(X_validation)
+        print(accuracy_score(Y_validation, predictions))
+        print(confusion_matrix(Y_validation, predictions))
+        print(classification_report(Y_validation, predictions))
+
+
 def runModelsRegression():
     array = dataset.values
     X = array[:,3:]
@@ -106,57 +165,6 @@ def runModelsRegression():
     #plt.yticks(())
 
     plt.show()
-
-def modelSurfaceStatus():
-    # Split-out validation dataset
-    array = dataset.values
-    X = customlib.sliceSkip2d(array, [0,2,6,7])
-    Y = array[:,7]  
-    validation_size = 0.2
-    seed = 7
-    X_train, X_validation,Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
-
-    # Test options and evaluation metric
-    seed = 7
-    scoring = 'accuracy'
-
-    # Spot Check Algorithms
-    models = []
-    models.append(('LR', LogisticRegression()))
-    models.append(('LDA', LinearDiscriminantAnalysis()))
-    models.append(('KNN', KNeighborsClassifier()))
-    models.append(('CART', DecisionTreeClassifier()))
-    models.append(('NB', GaussianNB()))
-    #models.append(('SVM', SVC())) # long run time...
-    # evaluate each model in turn
-    results = []
-    names = []
-    for name, model in models:
-        kfold = model_selection.KFold(n_splits=10, random_state=seed)
-        cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
-        results.append(cv_results)
-        names.append(name)
-        msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
-        print(msg)
-
-    # Compare Algorithms
-    fig = plt.figure()
-    fig.suptitle('Algorithm Comparison')
-    ax = fig.add_subplot(111)
-    plt.boxplot(results)
-    ax.set_xticklabels(names)
-    plt.show()
-
-    # Make predictions on validation dataset
-    #knn = KNeighborsClassifier()
-    #knn.fit(X_train, Y_train)
-    #predictions = knn.predict(X_validation)
-    cart = DecisionTreeClassifier()
-    cart.fit(X_train, Y_train)
-    predictions = cart.predict(X_validation)
-    print(accuracy_score(Y_validation, predictions))
-    print(confusion_matrix(Y_validation, predictions))
-    print(classification_report(Y_validation, predictions))
 
 #runModels()
 #runModelsRegression()
