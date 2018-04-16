70cf3a436753f666b218d66c3f721285ab1d3216
Tobias <tobias91.axelsson@gmail.com>
2018-04-16 21:02:55 +0200

    new branch, fewer refs

--- implementation/1520_2015-2016.py
+++ implementation/1520_2015-2016.py
@@ -12,6 +12,9 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
+from sklearn.neural_network import MLPClassifier
+from sklearn import svm
+from sklearn.ensemble import RandomForestClassifier
 
 from sklearn.linear_model import LinearRegression
 #own library
@@ -24,7 +27,7 @@ from sklearn.metrics import mean_squared_error, r2_score
 
 # Load dataset
 #xl = pd.ExcelFile("../data/stations_e6/mätvärden 1520 2015-2016.xlsx")
-dataset = pandas.read_excel(open('../data/stations_e6/mätvärden total.xlsx','rb'), sheet_name='dsc111_noerr', skiprows=[0], header=1, 
+dataset = pandas.read_excel(open('../data/stations_e6/mätvärden 1520 2015-2016.xlsx','rb'), sheet_name='ms4st_oversampling_test', skiprows=[0], header=1, 
     names=['År', 'Tidpunkt', 'Yttemp-MS4', 'Nedtyp-MS4', 'Ned mängd-MS4', 'Yttemp-DST111', 'Friktion-DSC111', 'Ytstatus-DSC111'],
     dtype={'a':np.int64, 'b':np.int64, 'c':np.float64, 'd':np.int64, 'e':np.float64, 'f':np.float64, 'g':np.float64, 'h':np.int64}
     #dtype=object
@@ -46,7 +49,7 @@ dataset = pandas.read_excel(open('../data/stations_e6/mätvärden total.xlsx','r
 #print(dataset.describe())
 
 # class distribution
-print(dataset.groupby('Ytstatus-DSC111').size())
+print(dataset.groupby('Nedtyp-MS4').size())
 
 #print(dataset.values)
 # box and whisker plots
@@ -83,7 +86,65 @@ def modelSurfaceStatus():
     models.append(('KNN', KNeighborsClassifier()))
     models.append(('CART', DecisionTreeClassifier()))
     models.append(('NB', GaussianNB()))
+    models.append(('BP', MLPClassifier()))
+    models.append(('SVM', SVC())) # long run time...
+
+    # evaluate each model in turn
+    results = []
+    names = []
+    for name, model in models:
+        kfold = model_selection.KFold(n_splits=10, random_state=seed)
+        cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+        results.append(cv_results)
+        names.append(name)
+        msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+        print(msg)
+
+    # Compare Algorithms
+    fig = plt.figure()
+    fig.suptitle('Algorithm Comparison')
+    ax = fig.add_subplot(111)
+    plt.boxplot(results)
+    ax.set_xticklabels(names)
+    plt.show()
+
+    # Make predictions on validation dataset
+    #knn = KNeighborsClassifier()
+    #knn.fit(X_train, Y_train)
+    #predictions = knn.predict(X_validation)
+    for name, model in models:
+        #cart = DecisionTreeClassifier()
+        print(name)
+        model.fit(X_train, Y_train)
+        predictions = model.predict(X_validation)
+        print(accuracy_score(Y_validation, predictions))
+        print(confusion_matrix(Y_validation, predictions))
+        print(classification_report(Y_validation, predictions))
+
+def modelPrecipationType():
+    # Split-out validation dataset
+    array = dataset.values
+    X = customlib.sliceSkip2d(array, [0,1,2,3,4])
+    Y = array[:, 3]
+    validation_size = 0.2
+    seed = 7
+    X_train, X_validation,Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
+
+    # Test options and evaluation metric
+    seed = 7
+    scoring = 'accuracy'
+
+    # Spot Check Algorithms
+    models = []
+    models.append(('LR', LogisticRegression()))
+    models.append(('LDA', LinearDiscriminantAnalysis()))
+    models.append(('KNN', KNeighborsClassifier()))
+    models.append(('CART', DecisionTreeClassifier()))
+    models.append(('NB', GaussianNB()))
+    #models.append(('BP', MLPClassifier()))
     #models.append(('SVM', SVC())) # long run time...
+    models.append(('RF', RandomForestClassifier()))
+
     # evaluate each model in turn
     results = []
     names = []
@@ -168,4 +229,5 @@ def runModelsRegression():
 
 #runModels()
 #runModelsRegression()
-modelSurfaceStatus()
+#modelSurfaceStatus()
+modelPrecipationType()
